0c273bb2cd64b3867e931d67a8a54b5d14ca6a3e
drobaxa <drobakhabohdan@gmail.com>
2023-06-05 20:38:44 +0200

    dodanie klasy Bank i dodanie funkcji wplata

---
+++ src/main/java/org/example/Bank.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Bank {
+    double bilans = 0;
+    public double deposit(double wplata) {
+        return bilans = bilans + wplata;
+
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.add(4,7));
-        System.out.println(calculator.sub(4,2.5));
+        Bank bank = new Bank();
+        bank.deposit(1000);
+        System.out.println(bank.deposit(1000));
     }
 }
