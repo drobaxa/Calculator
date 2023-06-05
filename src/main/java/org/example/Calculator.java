ba67e58e8fbc0e92b0ccc7a540267395af99eb58
drobaxa <drobakhabohdan@gmail.com>
2023-06-05 20:02:43 +0200

    dodanie klasy kalkulator

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,6 +3,11 @@ package org.example;
 public class Calculator {
 
     public int add(int x, int y){
+
         return x + y;
+    }
+    public double sub(double x, double y){
+        return x - y;
+
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println(calculator.add(4,7));
+        System.out.println(calculator.sub(4,2.5));
     }
 }
